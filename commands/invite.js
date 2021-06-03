c3b012a97c68bff48929dcdd7c6606563d623b1f
Hydroxy <52876200+HydroxyDev@users.noreply.github.com>
2021-06-03 11:43:08 +0200

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -14,7 +14,7 @@ async function giveanswer(msg) {
         .setColor('#00adee')
         .setTitle('Invite link')
         .setDescription('[Invite link](https://discord.com/oauth2/authorize?client_id=842470142704746526&permissions=2147929152&scope=bot)')
-        .setThumbnail('https://cdn.discordapp.com/avatars/842470142704746526/07f94c25d5e9b6d67c4879a2d40e3231.png?size=1024')
+        .setThumbnail('https://cdn.discordapp.com/avatars/842470142704746526/8bf2a91412a6803f2c8f69ada3a94987.png?size=1024')
         .setTimestamp()
         .setFooter('By Hydroxy#2491');
 
